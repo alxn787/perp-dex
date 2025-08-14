9d1536fa2f395ed80bbdbd2d1901c575e8bdfc12
alxn787 <alenabraham787@gmail.com>
2025-08-14 19:16:59 +0530

    usermap change

--- programs/perp-dex/src/states/user_map.rs
+++ programs/perp-dex/src/states/user_map.rs
@@ -3,18 +3,18 @@ use std::collections::BTreeMap;
 use anchor_lang::prelude::*;
 use crate::states::user::User;
 
-pub struct UserMap(pub BTreeMap<Pubkey, Account<User>>);
+pub struct UserMap (pub BTreeMap<Pubkey, User>);
 
-impl UserMap<'a> {
-    pub fn get_ref(&self, user_key: &Pubkey) -> Option<&Account<User>> {
+impl UserMap {
+    pub fn get_ref(&self, user_key: &Pubkey) -> Option<&User> {
         self.0.get(user_key)
     }
 
-    pub fn get_mut(&mut self, user_key: &Pubkey) -> Option<&mut Account<User>> {
+    pub fn get_mut(&mut self, user_key: &Pubkey) -> Option<&mut User> {
         self.0.get_mut(user_key)
     }
 
-    pub fn insert(&mut self, user_key: Pubkey, user: Account<User>) {
+    pub fn insert(&mut self, user_key: Pubkey, user: User) {
         self.0.insert(user_key, user);
     }
 
